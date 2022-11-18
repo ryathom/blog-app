2fd29bf05cee4ef4b2287fdca22bebe433eff11a
Ryan Thomas <ryanthomasj15@gmail.com>
2022-11-18 20:19:57 +0000

    Root route to articles index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'articles#index'
+
   get '/articles', to: 'articles#index'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
